c7df1440bbc414eaeb94c55352692d50c9d4bd31
marciojosebaldo <marciojosebaldo@gmail.com>
2023-07-26 20:08:10 -0300

    Add files via upload

---
+++ src/test/java/com/ControleAcessoMundoSENAI/ControleAcesso/ControleAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControleAcessoMundoSENAI.ControleAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
