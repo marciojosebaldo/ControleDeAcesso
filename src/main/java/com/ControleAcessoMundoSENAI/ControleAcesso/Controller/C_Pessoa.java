05a376b2b2110dfe48d30d379bffffc70edabbb7
marciojosebaldo <marciojosebaldo@gmail.com>
2023-07-28 21:59:44 -0300

    Add files via upload

--- src/main/java/com/ControleAcessoMundoSENAI/ControleAcesso/Controller/C_Pessoa.java
+++ src/main/java/com/ControleAcessoMundoSENAI/ControleAcesso/Controller/C_Pessoa.java
@@ -2,6 +2,8 @@ package com.ControleAcessoMundoSENAI.ControleAcesso.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_Pessoa {
@@ -11,4 +13,9 @@ public class C_Pessoa {
         return "Login/login";
     }
 
+    @PostMapping("/")
+    public String loginPessoa(@RequestParam("usuario") String usuario,
+                              @RequestParam("senha") String senha) {
+        return "Home/home";
+    }
 }
