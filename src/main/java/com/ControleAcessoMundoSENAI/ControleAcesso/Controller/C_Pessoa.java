9f55d9e8b3a9c56642f88a707ab718b9fffd692f
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-09 21:42:55 -0300

    HTML

--- src/main/java/com/ControleAcessoMundoSENAI/ControleAcesso/Controller/C_Pessoa.java
+++ src/main/java/com/ControleAcessoMundoSENAI/ControleAcesso/Controller/C_Pessoa.java
@@ -3,6 +3,7 @@ package com.ControleAcessoMundoSENAI.ControleAcesso.Controller;
 import com.ControleAcessoMundoSENAI.ControleAcesso.Model.M_Resposta;
 import com.ControleAcessoMundoSENAI.ControleAcesso.Service.S_Pessoa;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,8 +33,14 @@ public class C_Pessoa {
                                @RequestParam("telefone") String telefone,
                                @RequestParam("email") String email,
                                @RequestParam("senha") String senha,
-                               @RequestParam("confSenha") String confSenha) {
+                               @RequestParam("confSenha") String confSenha, Model model) {
         M_Resposta m_resposta = S_Pessoa.cadastrarPessoa(nome, cpf, email, telefone,senha, confSenha);
-        return "redirect:/";
+
+        if(m_resposta.getStatus()) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("mensagem",m_resposta.getMensagem());
+            return "Cad_Pessoa/cad_pessoa";
+        }
     }
 }
